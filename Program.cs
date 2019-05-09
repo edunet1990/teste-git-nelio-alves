2834bb231eaf126620c8a08614ed7a1e39f7271e
Nelio Alves <eduardosilva-9@hotmail.com>
2019-05-09 09:26:57 -0300

    Projeto inciado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
+
+namespace Testes
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            int total = (a > b) ? a += 10 : b += 10;
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+        
+    }
+    public class Employee
+    {
+    }
+
+}
